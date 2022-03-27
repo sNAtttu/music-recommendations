888f2dca81bd8fbecd91e7be7fddf4d9ed24f191
DESKTOP-TV0H05D\nsant <nsanteri@hotmail.com>
2022-03-27 14:06:12 +0300

    Initial commit with function and a tester script

---
+++ TestUtilities/call-function.js
@@ -0,0 +1,16 @@
+const axios = require("axios");
+
+const method = process.argv[2];
+const functionName = process.argv[3];
+const localBaseUrl = "http://localhost:7071/api";
+
+const url = `${localBaseUrl}/${functionName}`;
+
+const requestOptions = {
+    method,
+    url,
+}
+axios(requestOptions).then(response => {
+    console.log(response.status);
+    console.log(response.data);
+});
