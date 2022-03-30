98f54c6b725333a165a24d0b955db92a8fcc06bc
DESKTOP-TV0H05D\nsant <nsanteri@hotmail.com>
2022-03-30 22:34:00 +0300

    Added simple caching mechanism

--- AuthorizationFunction/index.ts
+++ AuthorizationFunction/index.ts
@@ -1,10 +1,13 @@
 import axios, { Axios, AxiosRequestConfig } from 'axios';
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
+let authToken = "";
+
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('Requesting authentication token');
     const clientId = process.env['SPOTIFY_CLIENT_ID'];
     const clientSecret = process.env['SPOTIFY_CLIENT_SECRET'];
+
 
     const serialize = obj => {
         var str = [];
@@ -26,23 +29,34 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
             grant_type: 'client_credentials'
         }
       };
-
-    const authResponse = await axios.post(authOptions.url, serialize(authOptions.data), authOptions);
-
-    if(authResponse.status !== 200) {
+    
+    
+    if(authToken) {
+        context.log("Using auth token from the cache");
         context.res = {
             // status: 200, /* Defaults to 200 */
-            body: { statusCode: authResponse.status, message: authResponse.statusText }
+            body: { token: authToken }
         };
         context.done();
     }
-    const { access_token } = authResponse.data;
-
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: { token: access_token }
-    };
-    context.done();
+    else {
+        context.log("Auth token not in cache, fetching a new one");
+        const authResponse = await axios.post(authOptions.url, serialize(authOptions.data), authOptions);
+        if(authResponse.status !== 200) {
+            context.res = {
+                // status: 200, /* Defaults to 200 */
+                body: { statusCode: authResponse.status, message: authResponse.statusText }
+            };
+            context.done();
+        }
+        const { access_token } = authResponse.data;
+        authToken = access_token;
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: { token: authToken }
+        };
+        context.done();
+    }
 };
 
 export default httpTrigger;
--- FetchRecommendationsSpotify/index.ts
+++ FetchRecommendationsSpotify/index.ts
@@ -1,11 +1,11 @@
 import axios, { Axios, AxiosRequestConfig } from 'axios';
 import { AzureFunction, Context, HttpRequest } from '@azure/functions';
 
+let genres = null;
+
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('Fetching recommendations from the Spotify');
     const authResponse = await axios.get(process.env["AUTHORIZATION_FUNCTION_URL"]);
     const { token } = authResponse.data;
-    
     const fetchGenresUrl = `https://api.spotify.com/v1/recommendations/available-genre-seeds`;
     const fetchUserConfig: AxiosRequestConfig = {
         headers: {
@@ -14,16 +14,26 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
             'Authorization': `Bearer ${token}`
         }
     }
-    const userResponse = await axios.get(fetchGenresUrl, fetchUserConfig);
-    if(userResponse.status !== 200) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: { statusCode: authResponse.status, message: authResponse.statusText }
-        };
+
+    if(genres) {
+        context.log("Returning genres from the cache");
+        context.res = { body: JSON.stringify(genres) }
         context.done();
     }
-    context.res = { body: JSON.stringify(userResponse.data.genres) }
-    context.done();
+    else {
+        context.log("There's no genres in the cache, refreshing from the API");
+        const genreResponse = await axios.get(fetchGenresUrl, fetchUserConfig);
+        if(genreResponse.status !== 200) {
+            context.res = {
+                // status: 200, /* Defaults to 200 */
+                body: { statusCode: authResponse.status, message: authResponse.statusText }
+            };
+            context.done();
+        }
+        genres = genreResponse.data.genres
+        context.res = { body: JSON.stringify(genreResponse.data.genres) }
+        context.done();
+    }
 };
 
 export default httpTrigger;
